185f2c80815b4a4680e9b53cc4c0fd0a0e64e47c
Rafael <rafazel@outlook.com>
2021-11-26 13:46:59 -0300

    modal-completo

--- js/script.js
+++ js/script.js
@@ -1,23 +1,42 @@
-const botaoAbrir=document.querySelector('[data-modal="abrir"]')
+const tabMenu=document.querySelectorAll('.js-tabmenu ul li')
+const tabContent = document.querySelectorAll('.js-tabcontent ul');
+const botaoAbrir=document.querySelectorAll('[data-modal="abrir"]')
 const botaoFechar=document.querySelector('[data-modal="fechar"]')
 const containerModal=document.querySelector('[data-modal="container"]')
 
+/*modal*/
 if(botaoAbrir && botaoFechar && containerModal){
-   function toggleModal(event){
+  function toggleModal(event){
+    event.preventDefault()
+    containerModal.classList.toggle('modal-ativo')
+  }
 
-      event.preventDefault()
-      containerModal.classList.toggle('ativo')
-   }
+  function cliqueForaModal(event){
+    if (event.target == this)
+    toggleModal(event)
+  }
 
-
-   function cliqueForaModal(event){
-      if(event.target===this)
-      toggleModal(event)
-   }
-
-   botaoAbrir.addEventListener('click', toggleModal)
-   botaoFechar.addEventListener('click', toggleModal)
-   containerModal.addEventListener('click', cliqueForaModal)
+    botaoAbrir.forEach((abrir)=>{
+      abrir.addEventListener('click',()=>{
+        toggleModal(event)
+      })
+    })
+    botaoFechar.addEventListener('click',toggleModal)
+    containerModal.addEventListener('click',cliqueForaModal)
 }
 
-initModal();
+/*mudar imagens*/
+if(tabMenu.length && tabContent.length) {
+function activeTab(index) {
+  tabContent.forEach((ul) => {
+    ul.classList.remove('modal-ativo');
+  });
+  tabContent[index].classList.add('modal-ativo');
+}
+
+tabMenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener('click', () => {
+    activeTab(index);
+  });
+});
+}
